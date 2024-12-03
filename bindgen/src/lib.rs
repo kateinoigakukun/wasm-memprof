c00a468a360678202c5b7ee9458b0f4fcfda9f2d
Yuta Saito <kateinoigakukun@gmail.com>
2024-12-03 16:53:14 +0900

    Install logger and panic handler at most once

--- bindgen/src/lib.rs
+++ bindgen/src/lib.rs
@@ -14,18 +14,40 @@ impl log::Log for Logger {
     }
 
     fn log(&self, record: &log::Record) {
-        log(&format!("[wasm-memprof][{}] {}", record.level(), record.args()));
+        log(&format!(
+            "[wasm-memprof][{}] {}",
+            record.level(),
+            record.args()
+        ));
     }
 
     fn flush(&self) {}
 }
 
-#[wasm_bindgen]
-pub fn instrument_allocator(bytes: Vec<u8>) -> Result<Vec<u8>, JsValue> {
+static mut INITIALIZED: bool = false;
+
+fn init_once() {
+    if unsafe { INITIALIZED } {
+        return;
+    }
+    unsafe {
+        INITIALIZED = true;
+    }
     log::set_logger(&Logger).unwrap();
     log::set_max_level(log::LevelFilter::Info);
+    std::panic::set_hook(Box::new(|info| {
+        log(&format!("Panic: {:?}", info));
+    }));
+}
+
+#[wasm_bindgen]
+pub fn instrument_allocator(bytes: Vec<u8>) -> Result<Vec<u8>, JsValue> {
+    init_once();
     match instrumentor::instrument(bytes, &instrumentor::allocator_tracees()) {
         Ok(instrumented) => Ok(instrumented),
-        Err(e) => Err(JsValue::from_str(&format!("Failed to instrument WebAssembly module: {}", e))),
+        Err(e) => Err(JsValue::from_str(&format!(
+            "Failed to instrument WebAssembly module: {}",
+            e
+        ))),
     }
 }
