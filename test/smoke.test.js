8e778dc05791f1075b0967861d5b66671da34759
Yuta Saito <kateinoigakukun@gmail.com>
2024-12-03 19:53:12 +0900

    Fix the smoke test to check the raw pprof output

--- test/smoke.test.js
+++ test/smoke.test.js
@@ -4,9 +4,12 @@ import * as path from "path"
 import test from 'node:test'
 import { WASI } from "wasi"
 import { execFileSync } from "child_process"
+import assert from "node:assert"
 
 test("smoke test", async () => {
-    const WebAssembly = WMProf.wrap(globalThis.WebAssembly)
+    const WebAssembly = WMProf.wrap(globalThis.WebAssembly, {
+      sampleRate: 1
+    })
     const dirname = path.dirname(new URL(import.meta.url).pathname);
     const wasmBytes = fs.readFileSync(path.join(dirname, "../examples/a.out.wasm"))
     const wasi = new WASI({
@@ -17,9 +20,31 @@ test("smoke test", async () => {
     })
     wasi.start(instance)
     const pprof = WMProf.get(instance).snapshot();
-    const tmpdir = fs.mkdtempSync(path.join(dirname, "test-"))
+    const tmpdir = fs.mkdtempSync(path.join(dirname, "tmp-"))
     const pprofPath = path.join(tmpdir, "test.pprof")
     fs.writeFileSync(pprofPath, pprof)
-    execFileSync("go", ["tool", "pprof", pprofPath, "--text"])
+    const result = execFileSync("go", ["tool", "pprof", "--raw", pprofPath])
+
+    const expected = `PeriodType: space bytes
+Period: 1
+Samples:
+inuse_space/bytes
+          4: 1 2 3 4 5 6 
+          4: 7 8 3 4 5 6 
+          4: 1 9 8 3 4 5 6 
+Locations
+     1: 0x1f0a M=1 foo wasm://wasm/a.out.wasm-89890da2:1:0 s=0
+     2: 0x1e71 M=1 __original_main wasm://wasm/a.out.wasm-89890da2:1:0 s=0
+     3: 0x1fa6 M=1 _start wasm://wasm/a.out.wasm-89890da2:1:0 s=0
+     4: 0x0 M=1 start node:wasi:114:0 s=0
+     5: 0x0 M=1 run node:internal/test_runner/test:632:0 s=0
+     6: 0x0 M=1 startSubtest node:internal/test_runner/harness:214:0 s=0
+     7: 0x1eb6 M=1 bar wasm://wasm/a.out.wasm-89890da2:1:0 s=0
+     8: 0x1e74 M=1 __original_main wasm://wasm/a.out.wasm-89890da2:1:0 s=0
+     9: 0x1ed3 M=1 bar wasm://wasm/a.out.wasm-89890da2:1:0 s=0
+Mappings
+1: 0x0/0x0/0x0   
+`
+    assert.strictEqual(result.toString(), expected)
     fs.rmSync(tmpdir, { recursive: true })
 })
